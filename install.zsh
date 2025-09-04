31e147b78088f6ffccf38de56b96075bcfcc2f86
matt-williams8 <13625722+matt-williams8@users.noreply.github.com>
2025-09-04 18:35:54 +1000

    Begin migration to mise for tool versions
    Install mise in install script
    Use mise to install python in install script in place of pyenv
    Add mise local tomls to gitignore
    Add permanent mise configuration to zsh dotfiles

--- install.zsh
+++ install.zsh
@@ -7,21 +7,18 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # Install git and clone the rest of this repo to get the ansible playbooks
 brew install git
 git clone https://github.com/matt-williams8/macos-setup.git $HOME/src/macos-setup
+cd $HOME/src/macos-setup
 
-# Install pyenv and temporarily add it to the path of the current shell so we can better manage python and pip versions
-brew install pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-eval "$(pyenv init --path)"
-
-# Install a more up-to-date version of python than is shipped with macOS
-PYTHON_VERSION=3.10.1
-pyenv install $PYTHON_VERSION
-pyenv global $PYTHON_VERSION
-
-# Install ansible and temporarily add pip packages to path so we can invoke it to run the playbooks
-python -m pip install --user ansible
+# Ideally mise would be managed by ansible as well, but we will use it to install other tools that are needed to install ansible itself.
+# We will only temporarily activate it add it to the path of the current shell, all permanent configuration will be done in the ansible playbook
+/bin/bash -c "$(curl https://mise.run | sh)"
 export PATH=$HOME/.local/bin:$PATH
 
-# Kick things off
-ansible-galaxy install -r requirements.yml
-ansible-playbook --skip-tags sdkman_privilege $HOME/src/macos-setup/macos.yml
+# Install python 3 but only configured in a local toml (that won't be committed) to avoid conflicts with global python settings
+mise use --path mise.local.toml python@3
+
+# Seemed to need to activate after installing python, but unclear why
+eval "$(mise activate bash)"
+
+python -m pip install --user ansible
+ansible-playbook $HOME/src/macos-setup/macos.yml
